29ac5d196c66d2fa19874f035ce4b54fe0d7564e
guomengtao <rinuo@qq.com>
2020-01-09 19:54:55 +0800

    mkdir src for a autoload

---
+++ src/learngit.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace learngit;
+
+class learngit{
+
+    /**
+     * 返回数字a和b的和
+     * @param int $a      数字
+     * @param int $b      数字
+     */
+
+	public static funciton add(int $a,int $ab){
+		return $a+$b;
+	}
+}
