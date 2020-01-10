8b96c8da70b42fe6b49537e8811e78f8ea890fbb
guomengtao <rinuo@qq.com>
2020-01-10 15:34:04 +0800

    move to libray root by count.php try run

---
+++ count.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace learngit;
+
+class count{
+
+    /**
+     * 返回数字a和b的和
+     * @param int $a      数字
+     * @param int $b      数字
+     */
+
+	public static function add(int $a,int $b){
+		return "count:".$a+$b;
+	}
+}
